406086df2ec7d3e0670eda54e48bb6870f31f225
Lilitha Mdlalana <jranonymous517@gmail.com>
2022-06-22 04:08:47 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <section>
+    <h1 class="text-white text-3xl font-bold text-center pt-3">
+      Vue Photo Gallery
+    </h1>
+    <div class="flex flex-col items-center">
+      <input type="text" v-model="value" class="focus:outline-none p-2 m-3" />
+      <button>Submit</button>
+    </div>
+  </section>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      value: "",
+    };
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  @apply bg-slate-500 w-full h-screen;
 }
 </style>
